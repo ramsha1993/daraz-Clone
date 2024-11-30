b46e1ddc4f9d40d5a8c510f68932b8d15473a058
ramsha1993 <rimshasaleem1993@gmail.com>
2024-11-30 12:09:57 +0500

    change

--- src/Components/home/Categories.jsx
+++ src/Components/home/Categories.jsx
@@ -22,7 +22,7 @@ import { Span } from '@chakra-ui/react'
 const Categories = () => {
 const data=[{id:1,image:'https://img.drz.lazcdn.com/static/pk/p/989928971444ffe65f62a2f6123ca641.png_170x170q80.png',text:'Mobile'},{id:2,image:'https://img.drz.lazcdn.com/g/kf/S01dee5ef12ec4dec82008e02f724c17bC.jpg_200x200q80.jpg_.webp',text:'Furniture'},{id:3,image:'https://img.drz.lazcdn.com/static/pk/p/78b32d8e7e28ce546309aa2acabe1c94.jpg_200x200q80.jpg_.webp',text:'Electronics'},{id:4,image:"https://img.drz.lazcdn.com/static/pk/p/81250c5dddd020a66824bd6aa2e52f9a.jpg_200x200q80.jpg_.webp",text:'Home Decor'},{id:5,image:"https://img.drz.lazcdn.com/static/pk/p/74fd7f293cb9d53e3f24582084b60507.jpg_200x200q80.jpg_.webp",text:'Health & Beauty'},{id:6,image:"https://img.drz.lazcdn.com/static/pk/p/70b454c00f809ea2d64878c6d7922bab.jpg_200x200q80.jpg_.webp",text:'Head phone'}]
   return (<>
-    <Box className={classes.categories} padding='6px 26px' >
+    <Box className={classes.categories} padding='6px 12px' >
         <h1>Categories</h1>
         <Grid templateColumns="repeat(6, 1fr)" className={classes.categoriesone}>
 {data.map((elem,index)=>(
@@ -72,10 +72,10 @@ const data=[{id:1,image:'https://img.drz.lazcdn.com/static/pk/p/989928971444ffe6
 </Box>
 <Box className={classes.resjust}>
 <Grid  templateColumns="repeat(2, 1fr)" templateRows="300px 300px
-300px 300px 300px " gap='18px' padding='10px 8px'>
+300px 300px 300px " gap='33px' padding='10px 8px'>
     {prod.filter((elem)=>(elem.id>39)).map((elem,index)=>
-       <GridItem key={index}  width='155px'>
-       <Box height='172px'><Image borderRadius='9px' src={elem.image} height='100%'/></Box>
+       <GridItem key={index}  width='161px'>
+       <Box height='180px'><Image borderRadius='9px' src={elem.image} height='100%'/></Box>
         <Box marginTop='7px'><Flex><Flex className={classes.fridayimg}><Image src={bigfriday} width='auto' height='auto' maxHeight="100%" maxWidth='100%' /></Flex><Text fontSize='14px' fontWeight='500' className={classes.para2}>{elem.description}</Text></Flex>
        <Flex color='#fe4960' alignItems='center'> <Span  fontSize='11px'>Rs.</Span><Span fontSize='22px' paddingBottom='4px'>{elem.price}</Span></Flex>
        <Flex ><Box textDecoration='line-through'  fontSize='12px' font-weight='500' color='gray'><Span>Rs.</Span> <Span>{elem.price*2}</Span></Box><Span fontSize='13px' font-weight='500' >-77%</Span></Flex>
