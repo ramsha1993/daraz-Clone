41efd1680b525b346166eeb66cd03719e13a9f43
ramsha1993 <rimshasaleem1993@gmail.com>
2024-11-30 12:33:51 +0500

    change

--- src/Components/home/Categories.jsx
+++ src/Components/home/Categories.jsx
@@ -74,8 +74,8 @@ const data=[{id:1,image:'https://img.drz.lazcdn.com/static/pk/p/989928971444ffe6
 <Grid  templateColumns="repeat(2, 1fr)" templateRows="300px 300px
 300px 300px 300px " gap='36px' padding='10px 8px'>
     {prod.filter((elem)=>(elem.id>39)).map((elem,index)=>
-       <GridItem key={index}  width='168px'>
-       <Box height='180px'><Image borderRadius='9px' src={elem.image} height='100%'/></Box>
+       <GridItem key={index}  width='160px'>
+       <Box height='164px'><Image borderRadius='9px' src={elem.image} height='100%'/></Box>
         <Box marginTop='7px'><Flex><Flex className={classes.fridayimg}><Image src={bigfriday} width='auto' height='auto' maxHeight="100%" maxWidth='100%' /></Flex><Text fontSize='14px' fontWeight='500' className={classes.para2}>{elem.description}</Text></Flex>
        <Flex color='#fe4960' alignItems='center'> <Span  fontSize='11px'>Rs.</Span><Span fontSize='22px' paddingBottom='4px'>{elem.price}</Span></Flex>
        <Flex ><Box textDecoration='line-through'  fontSize='12px' font-weight='500' color='gray'><Span>Rs.</Span> <Span>{elem.price*2}</Span></Box><Span fontSize='13px' font-weight='500' >-77%</Span></Flex>
